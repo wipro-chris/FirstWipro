71dc8a0b49e0a2d04eeae7794f54a1f1ed0ed215
wipro-chris <christopher.taylor.wipro.com>
2017-09-27 21:04:26 +0100

    Fundamentals 1

---
+++ Question1.java
@@ -0,0 +1,13 @@
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = args[0];
+		String str2 = args[1];  //Initialise strings and use arguments set in configurations
+
+		System.out.println(str1 + " " + str2); //Add the two strings together
+
+	}
+
+}
