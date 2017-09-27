a9fc7f6e54dc6ab5e6e3a2917941a9728fbc0f03
wipro-chris <christopher.taylor.wipro.com>
2017-09-27 21:07:00 +0100

    Fundamentals 1

---
+++ Question2.java
@@ -0,0 +1,17 @@
+
+public class Question2 {
+	
+	public static void main(String[] args){
+
+	String str1 = args[0];
+	String str2 = args[1]; //Initialise strings and use arguments set in configurations
+	
+	if (str1.equals(str2)){ // If the two strings match, do the following
+		System.out.println("These strings do match"); //print out statement
+	}
+	else	System.out.println("These strings do not match"); //if they do not match, print statement
+
+
+
+	}
+}
