60e8744cdfc0fecfac0a80556687a91c46015abb
wipro-chris <christopher.taylor.wipro.com>
2017-09-27 21:09:59 +0100

    Fundamentals 1

---
+++ Question4.java
@@ -0,0 +1,14 @@
+
+public class Question4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int str1 = Integer.parseInt(args[0]); 
+		int str2 = Integer.parseInt(args[1]);//initialise strings and pass them to an integer value
+		
+		System.out.println("The sum of " + str1 + " and " + str2 + " is " + (str1 + str2));
+		// add the two numbers together and print the total
+	}
+
+}
